ac31c89123b76c4b36dc200d0502f8da667eabec
Nur Nadhira Aqilah B Mohd Shah <mohdshah@shay.ecn.purdue.edu>
2020-02-07 14:53:15 -0500

    lab2

--- lab2/lab2/apps/q2/consumer/consumer.c
+++ lab2/lab2/apps/q2/consumer/consumer.c
@@ -6,17 +6,61 @@
 
 void main (int argc, char *argv[])
 {
-	circ_buffer * buff;
+	circ_buffer * buf;
 	uint32 h_mem;
 	sem_t s_procs_completed;
 	lock_t buff_lock;
+	int i = 0;
+	char str[] = "Hello World";
 
 	//check for correct no of arg
+	if (argc != 4) {
+		Printf("Usage");
+		Printf(argv[0]);
+		Printf("<handle_to_shared_memory_page> <handle_to_page_mapped_semaphore<handle to lock>");
+		Exit();
+	}
+
 	//convert command line str to int
+	h_mem = dstrtol(argv[1], NULL, 10);
+	s_procs_completed = dstrtol(argv[2], NULL, 10);
+	buff_lock = dstrtol(argv[3], NULL, 10);
+
 	//map shared memory page into this process memory page
-	//aquire the lock for the process
-	//check if buffer is empty or not before removing a char
-	//remove a char if its not empty
-	//releas the lock
-	//signal semaphore tell that we're done
+	if ((buf = (circ_buffer *) shmat(h_mem)) == NULL) {
+		Printf("Could not map the virtual address");
+		Printf(argv[0]);
+		Printf("exiting\n");
+		Exit();
+	}
+	
+	while (i < dstrlen(str)) {
+		//aquire the lock for the process
+		if (lock_acquire(buff_lock) != SYNC_SUCCESS) {
+			Exit();
+		}
+		//check if buffer is empty or not before removing a char
+		if (buf->head == buf->tail) {
+			//buffer is empty
+		}
+		else {
+			//buffer is not full
+			Printf("Consumer %d removed: %c\n", getpid(), buf->array[buf->tail]);
+			i++;
+			buf->tail = (buf->tail + 1) % BUFFERSIZE;
+		}
+		//release the lock
+		if (lock_release(buf_lock) != SYNC_SUCCESS) {
+			Exit();
+		}
+	}
+
+	//signal semaphore that we're done
+	Printf("consumer: PID %d is complete.\n", getpid());
+	if (sem_signal(s_procs_completed) != SYNC_SUCCESS) {
+		Printf("Bad semaphore s_procs_completed (%d) in ", s_procs_completed);
+		Printf(argv[0]);
+		Printf("exiting..\n");
+		Exit();
+	}
 }
--- lab2/lab2/apps/q2/producer/producer.c
+++ lab2/lab2/apps/q2/producer/producer.c
@@ -7,18 +7,64 @@
 void main (int argc, char *argv[])
 {
 
-	circ_buffer * buff;
+	circ_buffer * buf;
 	uint32 h_mem;
 	sem_t s_procs_completed;
 	lock_t buff_lock;
+	int i = 0;
+	char str[] = "Hello World";
 
-	//check for number of arguments
-	//convert the command line str to int
-	//map shared memory page
-	//aquire the lock for producer
-	//add char to buff
-	//check if buff is full or not
-	//release the lock after char is add
+	//check for correct no of arg
+	if (argc != 4) {
+		Printf("Usage");
+		Printf(argv[0]);
+		Printf("<handle_to_shared_memory_page> <handle_to_page_mapped_semaphore<handle to lock>");
+		Exit();
+	}
+
+	//convert command line str to int
+	h_mem = dstrtol(argv[1], NULL, 10);
+	s_procs_completed = dstrtol(argv[2], NULL, 10);
+	buff_lock = dstrtol(argv[3], NULL, 10);
+
+	//map shared memory page into this process memory page
+	if ((buf = (circ_buffer *) shmat(h_mem)) == NULL) {
+		Printf("Could not map the virtual address");
+		Printf(argv[0]);
+		Printf("exiting\n");
+		Exit();
+	}
+	
+	while (i < dstrlen(str)) {
+		//aquire the lock for the process
+		if (lock_acquire(buff_lock) != SYNC_SUCCESS) {
+			Exit();
+		}
+		//check if buffer is full or not before adding a char
+		if (( (buf->head + 1) % BUFFERSIZE) == buf->tail) {
+			//buffer is full
+		}
+		else {
+			//buffer is not full
+			Printf("Producer %d inserted: %c\n", getpid(), str[i]);
+			buf->array[buf->head] = str[i];
+			buf->head = (buf->head + 1) % BUFFERSIZE;
+			i = i + 1;
+		}
+		
+		//release the lock
+		if (lock_release(buff_lock) != SYNC_SUCCESS) {
+			Exit();
+		}
+	}
+
 	//signal semaphore that we're done
+	Printf("producer: PID %d is complete.\n", getpid());
+	if (sem_signal(s_procs_completed) != SYNC_SUCCESS) {
+		Printf("Bad semaphore s_procs_completed (%d) in ", s_procs_completed);
+		Printf(argv[0]);
+		Printf("exiting..\n");
+		Exit();
+	}
 
 }
