38459d2770d7d6e042babb6c52151ec848c08acc
Anthony Y Hou <hou71@shay.ecn.purdue.edu>
2020-02-14 17:25:42 -0500

    Edited Makeprocs.c

--- lab2/apps/q5/makeprocs/makeprocs.c
+++ lab2/apps/q5/makeprocs/makeprocs.c
@@ -6,20 +6,20 @@
 
 void main (int argc, char *argv[])
 {
-  int num_H2O = 0;               // Used to store number of H20 to create
+  int num_H2O = 0;               // Used to store number of H2O to create
   int num_SO4 = 0;               // Used to store number of SO4 to create
   int i;                          // Loop index variable
   
-  uint32 h_mem;                   // Used to hold handle to shared memory page
-  //sem_t s_procs_completed;        // Semaphore used to wait until all spawned processes have completed
-  sem_t H2O; //semaphore used to signal a H2O is created
-  sem_t SO4; //semaphore used to signal a SO4 is created
-  sem_t H2; //semaphore used to signal a H2 is created
-  sem_t O2; //semaphore used to signal a O2 is created
-  sem_t SO2; //semaphore used to singal a SO2 is created
-  sem_t H2SO4; //semaphore used to signal a H2SO4 is created
-  char h_mem_str[10];             // Used as command-line argument to pass mem_handle to new processes
-  //char s_procs_completed_str[10]; // Used as command-line argument to pass page_mapped handle to new processes
+  sem_t s_procs_completed;      //Semaphore used to wait until all spawned processes have completed
+  sem_t H2O; 			//semaphore used to signal a H2O is created
+  sem_t SO4; 			//semaphore used to signal a SO4 is created
+  sem_t H2; 			//semaphore used to signal a H2 is created
+  sem_t O2; 			//semaphore used to signal a O2 is created
+  sem_t SO2; 			//semaphore used to singal a SO2 is created
+  sem_t H2SO4; 			//semaphore used to signal a H2SO4 is created
+
+
+  char s_procs_completed_str[10]; // Used as command-line argument to pass page_mapped handle to new processes
   char H2O_str[10];
   char SO4_str[10];
   char H2_str[10];
@@ -33,17 +33,11 @@ void main (int argc, char *argv[])
   }
   
   // Convert string from ascii command line argument to integer number
-  num_H20 = dstrtol(argv[1], NULL, 10); // the "10" means base 10
+  num_H2O = dstrtol(argv[1], NULL, 10); // the "10" means base 10
   num_SO4 = dstrtol(argv[2], NULL, 10);
-  Printf("Creating %d H20s and %d S04s\n", num_H20, num_SO4);
+  Printf("Creating %d H2Os and %d S04s\n", num_H2O, num_SO4);
 
-  // Allocate space for a shared memory page, which is exactly 64KB
-  // Note that it doesn't matter how much memory we actually need: we 
-  // always get 64KB
-  if ((h_mem = shmget()) == 0) {
-    Printf("ERROR: could not allocate shared memory page in "); Printf(argv[0]); Printf(", exiting...\n");
-    Exit();
-  }
+  int numprocs = 5;
 
   // Create semaphore to not exit this process until all other processes 
   // have signalled that they are complete.  To do this, we will initialize
@@ -56,21 +50,84 @@ void main (int argc, char *argv[])
     Exit();
   }
 
+  //Create a Semaphore for each Molecule
+  //H2O Sem
+  if ((H2O = sem_create(0)) == SYNC_FAIL) {
+    Printf("Bad sem_create for H2O"); Printf(argv[0]); Printf("\n");
+    Exit();
+  }
+
+  //SO4 Sem
+  if ((SO4 = sem_create(0)) == SYNC_FAIL) {
+    Printf("Bad sem_create for SO4"); Printf(argv[0]); Printf("\n");
+    Exit();
+  }
+
+  //H2 Sem
+  if ((H2 = sem_create(0)) == SYNC_FAIL) {
+    Printf("Bad sem_create for H2"); Printf(argv[0]); Printf("\n");
+    Exit();
+  }
+
+  //O2 Sem
+  if ((O2 = sem_create(0)) == SYNC_FAIL) {
+    Printf("Bad sem_create for O2"); Printf(argv[0]); Printf("\n");
+    Exit();
+  }
+
+  //SO2 Sem
+  if ((SO2 = sem_create(0)) == SYNC_FAIL) {
+    Printf("Bad sem_create for SO2"); Printf(argv[0]); Printf("\n");
+    Exit();
+  }
+
+  //H2SO4 Sem
+  if ((H2SO4 = sem_create(0)) == SYNC_FAIL) {
+    Printf("Bad sem_create for H2SO4"); Printf(argv[0]); Printf("\n");
+    Exit();
+  }
+
+
   // Setup the command-line arguments for the new process.  We're going to
   // pass the handles to the shared memory page and the semaphore as strings
   // on the command line, so we must first convert them from ints to strings.
-  ditoa(h_mem, h_mem_str);
   ditoa(s_procs_completed, s_procs_completed_str);
-  ditoa(buff_lock, str_buff_lock);
+  ditoa(H2O, H2O_str);
+  ditoa(SO4, SO4_str);
+  ditoa(H2, H2_str);
+  ditoa(O2, H2O_str);
+  ditoa(SO2, SO2_str);
+  ditoa(H2SO4, H2SO4_str);
+
+
+
+
+
+  char H2O_str[10];
+  char SO4_str[10];
+  char H2_str[10];
+  char O2_str[10];
+  char SO2_str[10];
+  char H2SO4_str[10];
+
+  //Calculate how many times each process needs to iterate
+  int numInject_H2O 	= num_H2O;
+  int numInject_SO4 	= num_SO4;
+  int numReact1		= int(num_H2O / 2);
+  int numReact2		= num_SO4;
+  int numReact3		= min(2*numInject_H2O, min(numInject_H2O + num_SO4, num_SO4)); 
+
 
   // Now we can create the processes.  Note that you MUST :qend your call to
   // process_create with a NULL argument so that the operating system
   // knows how many arguments you are sending.
-  for(i=0; i<numprocs; i++) {
-    process_create(PRODUCER, h_mem_str, s_procs_completed_str, str_buff_lock, NULL);
-    process_create(CONSUMER, h_mem_str, s_procs_completed_str, str_buff_lock, NULL);
-    Printf("Processes %d created\n", i);
-  }
+  process_create(INJECT_H2O, s_procs_completed_str, H2O_str, numInject_H2O);
+  process_create(INJECT_SO4, s_procs_completed_str, SO4_str, numInject_SO4 );
+  process_create(REACTION_1, s_procs_completed_str, H2O_str, H2_str, O2_str, numReact1);
+  process_create(REACTION_2, s_procs_completed_str, SO4_str, SO2_str , O2_str, numReact2);
+  process_create(REACTION_3, s_procs_completed_str, H2_str, O2_str, SO2_str, H2SO4_str, numReact3);
+    
+  
 
   // And finally, wait until all spawned processes have finished.
   if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
