cdedb8601954992bff3de9a1ed1c6e13d7fac8a7
Nur Nadhira Aqilah B Mohd Shah <mohdshah@ecegrid-thin1.ecn.purdue.edu>
2020-04-14 00:48:05 -0400

    done the different scenarios

---
+++ lab4/one-level/apps/example/q2_4/q2_4.c
@@ -0,0 +1,35 @@
+#include "usertraps.h"
+#include "misc.h"
+
+int helper(int n){
+  if (n==0){
+    return 0;
+  }
+  return (1 + helper(n-1))
+}
+
+void main (int argc, char *argv[])
+{
+  sem_t s_procs_completed; // Semaphore to signal the original process that we're done
+  int temp = 400;
+
+  if (argc != 2) { 
+    Printf("Usage: %s <handle_to_procs_completed_semaphore>\n"); 
+    Exit();
+  } 
+
+  // Convert the command-line strings into integers for use as handles
+  s_procs_completed = dstrtol(argv[1], NULL, 10);
+
+  // Now print a message to show that everything worked
+  Printf("q2_4 (%d): cause the user function call stack to grow larger than 1 page\n", getpid());
+  helper(temp);
+
+  // Signal the semaphore to tell the original process that we're done
+  if(sem_signal(s_procs_completed) != SYNC_SUCCESS) {
+    Printf("q2_4 (%d): Bad semaphore s_procs_completed (%d)!\n", getpid(), s_procs_completed);
+    Exit();
+  }
+
+  Printf("q2_4 (%d): Done!\n", getpid());
+}
